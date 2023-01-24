7a88f72e6f79edbc70b330822191cff2f4d851ed
ClemencioMoralesTW <clemencio.m@thoughtworks.com>
2023-01-24 10:07:13 +0100

    ClemencioM - Refactors AreaCalculatorTest.java

--- src/test/java/open_closed/exercise_1/good/AreaCalculatorTest.java
+++ src/test/java/open_closed/exercise_1/good/AreaCalculatorTest.java
@@ -1,54 +1,44 @@
 package open_closed.exercise_1.good;
 
-import open_closed.exercise_1.good.shape.*;
+import open_closed.exercise_1.good.shape.Circle;
+import open_closed.exercise_1.good.shape.Hexagon;
+import open_closed.exercise_1.good.shape.Rectangle;
+import open_closed.exercise_1.good.shape.Shape;
+import open_closed.exercise_1.good.shape.Square;
+import open_closed.exercise_1.good.shape.Triangle;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AreaCalculatorTest {
 
     @Test
     void shouldCalculateTheAreaOfACircle() {
-        var circle = new Circle(5);
-
-        var result = AreaCalculator.calculateArea(circle);
-
-        assertEquals(78.54, result, 0.005);
+        Shape circle = new Circle(5);
+        assertEquals(78.54, AreaCalculator.calculateArea(circle), 0.005);
     }
 
     @Test
     void shouldCalculateTheAreaOfARectangle() {
-        var rectangle = new Rectangle(4, 5);
-
-        var result = AreaCalculator.calculateArea(rectangle);
-
-        assertEquals(20, result);
+        Shape rectangle = new Rectangle(4, 5);
+        assertEquals(20, AreaCalculator.calculateArea(rectangle));
     }
 
     @Test
     void shouldCalculateTheAreaOfASquare() {
-        var square = new Square(3);
-
-        var result = AreaCalculator.calculateArea(square);
-
-        assertEquals(9, result);
+        Shape square = new Square(3);
+        assertEquals(9, AreaCalculator.calculateArea(square));
     }
 
     @Test
     void shouldCalculateTheAreaOfATriangle() {
-        var triangle = new Triangle(10, 16, 25);
-
-        var result = AreaCalculator.calculateArea(triangle);
-
-        assertEquals(43.33, result, 0.005);
+        Shape triangle = new Triangle(10, 16, 25);
+        assertEquals(43.33, AreaCalculator.calculateArea(triangle), 0.005);
     }
 
     @Test
     void shouldCalculateTheAreaOfAHexagon() {
-        var hexagon = new Hexagon(5);
-
-        var result = AreaCalculator.calculateArea(hexagon);
-
-        assertEquals(64.95, result, 0.005);
+        Shape hexagon = new Hexagon(5);
+        assertEquals(64.95, AreaCalculator.calculateArea(hexagon), 0.005);
     }
 }
